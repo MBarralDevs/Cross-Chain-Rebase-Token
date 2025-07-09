d02cc5d9ea8a6903962baa8b1a2049c38a66e0ee
mbarraldevs <mbarraldevs@outlook.com>
2025-07-09 20:13:46 +0200

    Commit creation of BridgeToken script end

--- script/BridgeToken.s.sol
+++ script/BridgeToken.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.24;
 import {Script} from "forge-std/Script.sol";
 import {IRouterClient} from "@ccip/contracts/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {Client} from "@ccip/contracts/src/v0.8/ccip/libraries/Client.sol";
+import {IERC20} from "@ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 
 contract BridgeToken is Script {
     function run(
@@ -27,6 +28,13 @@ contract BridgeToken is Script {
             feeToken: linkTokenAddress, // The LINK token address is used to pay for the CCIP fees
             extraArgs: Client._argsToBytes(Client.EVMExtraArgsV1({gasLimit: 500_000})) // We set the gas limit for the callback on the destination chain
         });
+
+        //We need to get the fees for our ccip transaction
+        uint256 fee = IRouterClient(routerAddress).getFee(destinationChainSelector, message);
+
+        //To finish we need to approve our router address to spend the link and local tokens
+        IERC20(linkTokenAddress).approve(routerAddress, fee);
+        IERC20(localToken).approve(routerAddress, amountToBridge);
 
         //We call the ccipSend function to bridge the tokens
         IRouterClient(routerAddress).ccipSend(destinationChainSelector, message);
